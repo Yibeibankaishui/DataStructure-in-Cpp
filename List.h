2f4227f63d066799ccca39dc1a0efe9908df556e
yibeibankaishuimbp <yibeibankaishui@163.com>
2022-02-18 15:46:19 +0800

    fix few bugs

--- List.h
+++ List.h
@@ -21,7 +21,7 @@ class List{
     public:
         class const_iterator{
             public:
-                const_iterator(): current{nullptr} { }
+                const_iterator(): theList(nullptr), current(nullptr) { }
 
                 const Object & operator*() const{
                     return retrieve();
@@ -50,11 +50,11 @@ class List{
 
                 Node * current;
 
-                Object * retrieve() const{
+                Object & retrieve() const{
                     return current -> data;
                 }
 
-                const_iterator(const List<Object> & lst, Node *p): theList{&lst}, current{p} { }
+                const_iterator(const List<Object> & lst, Node *p): theList(&lst), current(p) { }
 
                 void assertIsValid() const{
                     if(theList == nullptr || current == nullptr || current == theList -> head)
@@ -89,7 +89,7 @@ class List{
 
             protected:
                 // iterator(Node *p): const_iterator{p} { }
-                iterator(const List<Node> & lst, Node *p): const_iterator{lst, p} { }                
+                iterator(const List<Object> & lst, Node *p): const_iterator(lst, p) { }                
 
                 friend class List<Object>;
         };
@@ -118,7 +118,7 @@ class List{
             return *this;
         }
 
-        List(List && rhs): theSize{rhs.theSize}, head{rhs.head}, tail{rhs.tail} {
+        List(List && rhs): theSize(rhs.theSize), head(rhs.head), tail(rhs.tail) {
             rhs.theSize = 0;
             rhs.head = nullptr;
             rhs.tail = nullptr;
@@ -235,7 +235,7 @@ class List{
 
             Node *p = itr.current;
             theSize++;
-            return {p -> prev = p -> prev -> next = new Node{x, p -> prev, p}};
+            return {*this, p -> prev = p -> prev -> next = new Node{x, p -> prev, p}};
         }
 
         // iterator insert(iterator itr, Object && x){
@@ -251,7 +251,7 @@ class List{
 
             Node *p = itr.current;
             theSize++;
-            return {p -> prev = p -> prev -> next = new Node{std::move(x), p -> prev, p}};
+            return {*this, p -> prev = p -> prev -> next = new Node{std::move(x), p -> prev, p}};
         }
 
         // iterator erase(iterator itr){
@@ -322,13 +322,14 @@ class ListSingle{
             Object data;
             Node *next;
 
-            Node(const Object & d = Object{ }, Node * n = nullptr): data{d}, next{n} { }
+            Node(const Object & d = Object{ }, Node * n = nullptr): data(d), next(n) { }
             Node(Object && d, Node * n = nullptr): data{std::move(d)}, next{n} { }
         };
 
     private:
         int theSize;
         Node *head;
+        Node *tail;
 
         void init(){
             theSize = 0;
@@ -338,39 +339,43 @@ class ListSingle{
         }
 
     public:
-        class const_iterator{ };
+        class const_iterator{
+
+        };
         
-        class iterator : public const_iterator{ };
+        class iterator : public const_iterator{
+
+        };
 
     public:
-        List(){
+        ListSingle(){
             init();
         }
 
-        List(const List & rhs){
+        ListSingle(const ListSingle & rhs){
             init();
             for(auto & x: rhs){
                 push_back(x);
             }
         }
         
-        ~List(){
+        ~ListSingle(){
             clear();
             delete head;
             delete tail;
         }
 
-        List & operator=(const List & rhs){
+        ListSingle & operator=(const ListSingle & rhs){
 
         }
 
-        List(List && rhs): theSize{rhs.theSize}, head{rhs.head}{
+        ListSingle(ListSingle && rhs): theSize(rhs.theSize), head(rhs.head){
             rhs.theSize = 0;
             rhs.head = nullptr;
             rhs.tail = nullptr;
         }
 
-        List & operator=(List && rhs){
+        ListSingle & operator=(ListSingle && rhs){
             std::swap(theSize, rhs.theSize);
             std::swap(head, rhs.head);
             std::swap(tail, rhs.tail);
@@ -423,29 +428,45 @@ class ListSingle{
         }
 
         void push_front(const Object & x){
-
+            insert(begin(), x);
         }
 
         void push_front(Object && x){
-
+            insert(begin(), std::move(x));
         }
 
         void push_back(const Object & x){
-
+            insert(end(), x);
         }
 
         void push_back(Object && x){
-
+            insert(end(), std::move(x));
         }
 
         void pop_front(){
-
+            erase(begin());
         }
 
         void pop_back(){
+            erase(--end());
+        }
+
+        iterator insert(iterator itr, const Object & x){
 
         }
-        
+
+        iterator insert(iterator itr, Object && x){
+
+        }
+
+        iterator erase(iterator itr){
+
+        }
+
+        iterator erase(iterator from, iterator to){
+
+        }
+
 };
 
 
