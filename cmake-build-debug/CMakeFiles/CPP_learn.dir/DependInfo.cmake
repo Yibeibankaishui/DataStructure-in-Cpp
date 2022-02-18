aa388863edb24497ce2ad0e5b67009bb366349f7
yibeibankaishuimbp <yibeibankaishui@163.com>
2022-02-18 15:46:27 +0800

    add Cmake

---
+++ cmake-build-debug/CMakeFiles/CPP_learn.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yibeibankaishui/Desktop/CPP_learn/ADT_list.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/ADT_list.cpp.o"
+  "/Users/yibeibankaishui/Desktop/CPP_learn/algorithms/PostfixNotation.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/algorithms/PostfixNotation.cpp.o"
+  "/Users/yibeibankaishui/Desktop/CPP_learn/algorithms/SymbolBalance.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/algorithms/SymbolBalance.cpp.o"
+  "/Users/yibeibankaishui/Desktop/CPP_learn/big_five_test.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/big_five_test.cpp.o"
+  "/Users/yibeibankaishui/Desktop/CPP_learn/copy_test.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/copy_test.cpp.o"
+  "/Users/yibeibankaishui/Desktop/CPP_learn/maintest.cpp" "/Users/yibeibankaishui/Desktop/CPP_learn/cmake-build-debug/CMakeFiles/CPP_learn.dir/maintest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
